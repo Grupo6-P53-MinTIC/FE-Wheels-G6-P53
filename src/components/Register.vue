1ab27962feaf377d1f2c51ccccd2f5533467c53f
william jimenez <>
2021-12-09 12:29:49 -0500

    prueba bootstrap

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -14,7 +14,7 @@
         <div class="mb-3">
           <input
             class="inputR"
-            v-model="user.username"
+            v-model="userP.username"
             type="text"
             placeholder="Nombre de usuario"
           />
@@ -22,7 +22,7 @@
         <div class="mb-3">
           <input
             class="inputR"
-            v-model="user.password1"
+            v-model="userP.password1"
             type="password"
             placeholder="Contraseña"
           />
@@ -30,11 +30,125 @@
         <div class="mb-3">
           <input
             class="inputR"
-            v-model="user.password2"
+            v-model="userP.password2"
             type="password"
             placeholder="Confirma contraseña"
           />
         </div>
+        <h4 class="text-center"><b>informacion personal</b></h4>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="userP.name"
+            type="text"
+            placeholder="nombre del pasagero"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="userP.lastName"
+            type="text"
+            placeholder="apellido del pasagero"
+          />
+        </div>
+        <p class="text-center">fecha de nacimiento:</p>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="userP.birthDate"
+            type="datetime-local"
+            id="meeting-time"
+            name="meeting-time"
+            min="2021-10-31T17:22:29Z"
+            max="2030-10-31T17:22:29Z"
+          />
+        </div>
+        <select v-model="userP.gender"  class="form-control" id="GenderInput">
+          <option value="">genero</option>
+          <option value="M">Maculino</option>
+          <option value="F">Femenino</option>
+        </select>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="userP.documentNumber"
+            type="text"
+            placeholder="numero de documento"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="userP.phoneNumber"
+            type="text"
+            placeholder="numero telefonico"
+          />
+        </div>
+        <h4 class="text-center"><b>Tipo de usuario</b></h4>
+
+        <div class="mb-3">
+          <b-button v-b-toggle.my-collapse>Conductor</b-button>
+          <b-button>Pasagero</b-button>
+        </div>
+<!-- BOTONES DE BOOSTRAP -->
+        <b-collapse id="my-collapse">
+          <b-card title="Collapsible card">
+            <div class="mb-3">
+              <input
+                class="inputR"
+                v-model="car.carRegistrationNumber"
+                type="text"
+                placeholder="placa del automobil"
+              />
+            </div>
+          </b-card>
+        </b-collapse>
+
+
+
+        
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="car.licenseNumber"
+            type="text"
+            placeholder="numero de licencia de conduccion"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="car.color"
+            type="text"
+            placeholder="color del vehiculo"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="car.brand"
+            type="text"
+            placeholder="marca del vehiculo"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="car.model"
+            type="text"
+            placeholder="modelo del vehiculo"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            class="inputR"
+            v-model="car.description"
+            type="text"
+            placeholder="descripcion del vehiculo"
+          />
+        </div>
+
         <div class="mb-3">
           <p class="text-center">¿Ya tienes una cuenta?</p>
           <p class="text-center mt-0" v-on:click="loadLogin">
@@ -52,7 +166,7 @@
       </form>
     </div>
   </div>
-  <!-- <div class="register">
+  <div class="register">
     <div class="container_register">
       <h2><b>Registrate</b></h2>
       <form v-on:submit.prevent="registerUser">
@@ -93,12 +207,30 @@ export default {
   name: "register",
   data: function () {
     return {
-      user: {
+      userP: {
         username: "",
         password: "",
         password2: "",
+        email: "",
+        name:"",
+        lastName:"",
+        birthDate:"",
+        gender:"",
+        documentNumber:"",
+        phoneNumber:"",
+        typeAccount:""
       },
-      error: false,
+      car:{
+        carRegistrationNumber:"",
+        licenseNumber:"",
+        color:"",
+        brand:"",
+        model:"",
+        description:"",
+        equipament:""
+      },
+      Driver: false,
+      error:  false,
     };
   },
   methods: {
@@ -120,7 +252,9 @@ export default {
         });
     },
   },
-  created: function () {},
+  created: function () {
+
+  },
 };
 </script>
 
